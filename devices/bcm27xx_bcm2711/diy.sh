168bb30708bca7c5d8e57683304f039fcec3d9b5
GUNGUN1-8 <fallout45515774@outlook.com>
2025-02-13 00:56:37 +0800

    Update diy.sh

--- devices/bcm27xx_bcm2711/diy.sh
+++ devices/bcm27xx_bcm2711/diy.sh
@@ -6,4 +6,4 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152/' target/linux/bcm27xx/Makefile
 
-
+#
